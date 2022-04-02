9f17d5d31831e55cc26f160d0090b15bf023fa55
seongho-joo <hazzyseong@gmail.com>
2022-04-02 21:17:57 +0900

    FEAT: 주문 리포지토리 및 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,12 +4,15 @@ import static javax.persistence.FetchType.LAZY;
 
 import javax.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -27,6 +30,7 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
